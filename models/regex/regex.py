3c648879d0fbc089cbc2874ca8852dcd83efd575
Matt Stammers <matthew.stammers@uhs.nhs.uk>
2025-06-22 23:56:14 +0100

    added regex model list

---
+++ models/regex/regex.py
@@ -0,0 +1,34 @@
+# Regex keywords for IBD and Drugs
+IBD_KEYWORDS = [
+    r"\bibd\b",
+    r"\binflammatory bowel disease\b",
+    r"\bcrohn'?s\b",
+    r"\bcolitis\b",
+    r"\bulcerative colitis\b",
+]
+
+DRUG_KEYWORDS = [
+    r"\bhydrocortisone\b",
+    r"\bprednisolone\b",
+    r"\boctasa\b",
+    r"\bsalofalk\b",
+    r"\bpentasa\b",
+    r"\basacol\b",
+    r"\bmesalazine\b",
+    r"\bbudenofalk\b",
+    r"\bbudesonide\b",
+    r"\bsulfasalazine\b",
+    r"\bsalazopyrin\b",
+    r"\btofacitinib\b",
+    r"\badalimumab\b",
+    r"\bmethotrexate\b",
+    r"\betanercept\b",
+    r"\bazathioprine\b",
+    r"\bmercaptopurine\b",
+    r"\bustekinumab\b",
+    r"\binfliximab\b",
+    r"\bvedolizumab\b",
+    r"\bmirikizumab\b",
+    r"\bupadacitinib\b",
+    r"\bfilgotinib\b",
+]
